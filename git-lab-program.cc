98adb20af089cc2838b71fa227482a1be150de60
newmanpj21 <pjn@lawsononline.net>
2021-09-01 18:26:07 -0400

    Added git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Pete Newman
+ *        Date: 9/1/2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
